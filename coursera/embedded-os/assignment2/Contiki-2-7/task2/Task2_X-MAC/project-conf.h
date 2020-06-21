c959219da056cfd270ef80e17f24cd1c0cf22feb
Joao Lourenco <lourencoccc@gmail.com>
2020-06-21 17:22:46 +0200

    reorder the files

---
+++ coursera/embedded-os/assignment2/Contiki-2-7/task2/Task2_X-MAC/project-conf.h
@@ -0,0 +1,14 @@
+
+#ifndef PROJECT_CONF_H_
+#define PROJECT_CONF_H_
+
+#undef NETSTACK_CONF_RDC_CHANNEL_CHECK_RATE
+#define NETSTACK_CONF_RDC_CHANNEL_CHECK_RATE 16
+
+#undef NETSTACK_CONF_RDC
+#define NETSTACK_CONF_RDC xmac_driver
+
+#undef NETSTACK_CONF_MAC
+#define NETSTACK_CONF_MAC nullmac_driver
+
+#endif /* PROJECT_CONF_H_ */
